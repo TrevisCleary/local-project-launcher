fe99713e956eca77e360a65fa698be69d526f414
Trevis Cleary <trevis.cleary@gmail.com>
2026-06-04 11:20:33 -0400

    Fix dev launcher hostname binding

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["dev-launcher", "devlauncher"],
     proxy: {
       "/api": "http://localhost:3059",
     },
